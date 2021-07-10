f29e6feb2a6140ae67cf9fb577f208799c811765
ebal5 <eval.scheme@gmail.com>
2021-07-11 00:08:55 +0900

    add variables for ir

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,32 @@
 #include "certificate.h"
 #include "secrets.h"
 
+#include <IRrecv.h>
+#include <IRsend.h>
+#include <IRutils.h>
+#include <IRremoteESP8266.h>
+
 String deviceID, groupID;
 uint32_t mqttGID, mqttDID;
 WiFiClientSecure net;
-MQTTClient client(16384);
+MQTTClient client(16384); /* TODO: バッファサイズの調整． */
+
+const uint16_t irRxPin = 14;
+const uint16_t irTxPin = 33;
+const uint16_t kCaptureBufferSize = 1024;
+
+#if DECODE_AC
+const uint8_t kTimeout = 50;
+#else  // DECODE_AC
+const uint8_t kTimeout = 15;
+#endif // DECODE_AC
+const uint16_t kMinUnknownSize = 12;
+const uint8_t kTolerancePercentage = kTolerance; // kTolerance is normally 25%
+
+// Use turn on the save buffer feature for more complete capture coverage.
+IRrecv irrecv(irRxPin, kCaptureBufferSize, kTimeout, true);
+decode_results results; // Somewhere to store the results
+IRsend irsend(irTxPin);
 
 void mqttCallBack(String &topic, String &payload)
 {
@@ -21,12 +43,20 @@ void setup()
   String user = MQTT_USER;
   String pass = MQTT_PASS;
   reconnectMqtt(&client, deviceID.c_str(), user.c_str(), pass.c_str());
-}
   
-void loop() {
+  assert(irutils::lowLevelSanityCheck() == 0);
+  Serial.printf("\n" D_STR_IRRECVDUMP_STARTUP "\n", irRxPin);
+  irrecv.setUnknownThreshold(kMinUnknownSize);
+  irrecv.setTolerance(kTolerancePercentage); 
+  irsend.begin();
+}
+
+void loop()
+{
   client.loop();
 
-  if (!client.connected()) {
+  if (!client.connected())
+  {
     Serial.println("Not connected");
     reconnectMqtt(&client, deviceID.c_str(), MQTT_USER, MQTT_PASS);
   }
